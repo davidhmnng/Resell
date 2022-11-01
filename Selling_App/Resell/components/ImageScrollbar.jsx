88519469d9d58ff5d12350869a32bca5f84638f3
Haomin Ng <87441477+davidhmnng@users.noreply.github.com>
2022-11-01 09:49:20 -0400

    Update ImageScrollbar.jsx

--- Selling_App/Resell/components/ImageScrollbar.jsx
+++ Selling_App/Resell/components/ImageScrollbar.jsx
@@ -1,8 +1,8 @@
 import { useContext } from 'react';
 import Image from 'next/image';
 import { Box, Icon, Flex } from '@chakra-ui/react';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+import { ScrollMenu, VisibilityContext } from 'horizontal-scrolling-menu';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'icons/fa';
 
 const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
