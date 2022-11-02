cfd17b932e8596077cfec46f1d32455121cef091
Haomin Ng <87441477+davidhmnng@users.noreply.github.com>
2022-11-02 02:08:26 -0400

    Update fetchApi.js

--- Selling_App/Resell/utils/fetchApi.js
+++ Selling_App/Resell/utils/fetchApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
-
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
